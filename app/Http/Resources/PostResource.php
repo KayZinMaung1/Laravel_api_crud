d6d9a4e06b42eea4a25ec62ab70e2f8f66d0556e
KayZinMaung1 <kayzinmg363@gmail.com>
2022-03-29 08:44:40 +0630

    modify resource file

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -14,6 +14,16 @@ class PostResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+       return [
+           'id'=> $this->id,
+           'title'=> $this->title,
+           'body'=>$this->body
+       ];
+    }
+    public function with($request){
+        return[
+            'version'=>'1.0.0',
+            'author_url'=>url('http://testapiurl.com')
+        ];
     }
 }
